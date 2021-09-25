4984963c4c38b80c26e93e3f7d549dc617fef93c
dishantsharma1988 <89789962+dishantsharma1988@users.noreply.github.com>
2021-09-25 20:08:36 +0530

    Update train (3).py

--- train (3).py
+++ train (3).py
@@ -194,8 +194,8 @@ class Classifier(nn.Module):
        
        # replaced self.hidden_layers= nn.ModuleList([nn.Linear(int(input_size),int(hidden_layers[0]))])
         #hlayers = zip(int(hidden_layers),int(hidden_layers))
-        zip(itertools.repeat((hidden_layers),(hidden_layers)))
-        #hlayers = zip(int(hidden_layers[:-1]),int(hidden_layers[1:]))
+        #zip(itertools.repeat((hidden_layers),(hidden_layers)))
+        hlayers = list(zip(hidden_layers[:-1],hidden_layers[1:])
         self.hidden_layers.extend([nn.Linear(int(hinput),int(houtput)) for hinput,houtput in hlayers])
         self.output = nn.Linear(hidden_layers[-1],output_size)
         self.dropout = nn.Dropout(p=drop_out)
